c3309a810c15e4fb77b7ca50a3857b4c3711231c
Xin <102579886+Xinslive@users.noreply.github.com>
2024-07-05 17:11:45 +0800

    Create ajax.js

---
+++ static/js/ajax.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function() {
+    loadPage(1);
+
+document.getElementById('pagination').addEventListener('click', function(event) {
+    if (event.target.classList.contains('page-link')) {
+            event.preventDefault();
+        const page = event.target.getAttribute('data-page');
+            loadPage(page);
+        }
+    });
+});
+
+function loadPage(page) {
+    const gallery = document.getElementById('gallery');
+    const pagination = document.getElementById('pagination');
+    const loadingIndicator = document.getElementById('loading-indicator');
+
+    gallery.innerHTML = '';
+    pagination.innerHTML = '';
+    loadingIndicator.style.display = 'block';
+    gallery.style.display = 'none';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'index.php?page=' + page, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            if (response.images.length > 0) {
+                response.images.forEach(image => {
+                    const imageContainer = document.createElement('div');
+                    imageContainer.classList.add('gallery-item');
+                    imageContainer.innerHTML = `
+                        <a href="${image.url}" class="glightbox">
+                            <img src="${image.url}" alt="Image" class="lazy-image" onerror="this.onerror=null;this.src='/static/svg/404.svg';" onclick="zoomImage(this)">
+                        </a>
+                        <button class="delete-btn" data-id="${image.id}" data-path="${image.path}"><img src="/static/svg/xmark.svg" alt="X" /></button>
+                        <button class="copy-btn" data-url="${image.url}"><img  src="/static/svg/link.svg" alt="Copy" /></button>
+                    `;
+                    gallery.appendChild(imageContainer);
+                });
+                gallery.style.display = 'block';
+                pagination.innerHTML = response.pagination;
+                lightbox.reload();
+                bindImageActions();
+            } else {
+                gallery.style.display = 'none';
+            }
+            loadingIndicator.style.display = 'none';
+        }
+    };
+    xhr.send();
+}
+
+document.getElementById('scroll-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', function() {
+    const scrollToTop = document.getElementById('scroll-to-top');
+    if (window.scrollY > 100) {
+        scrollToTop.style.display = 'block';
+    } else {
+        scrollToTop.style.display = 'none';
+    }
+});
