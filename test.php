4e47ed6a9185bf548be952af37e47f17dfa3bd31
Xin <102579886+Xinslive@users.noreply.github.com>
2024-08-22 20:16:31 +0800

    Delete test.php

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$imagick = new Imagick();
-echo "ImageMagick Version: " . $imagick->getVersion()['versionString'] . "\n";
-echo "Supported Formats: \n";
-print_r($imagick->queryFormats('*'));
-?>
