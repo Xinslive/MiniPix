82781f7d9afa366e407c8af6c83a4771268f5ac6
Xin <102579886+Xinslive@users.noreply.github.com>
2024-09-12 19:33:55 +0800

    Update api-avif.php

--- api-avif.php
+++ api-avif.php
@@ -300,8 +300,8 @@ function getStorage($storage) {
             if (!class_exists($ossClientClass) || !class_exists($ossExceptionClass)) {
                 throw new Exception("OSS 类未加载");
             }
-            $ossClient = new $ossClientClass($config['accessKeyId'], $config['accessKeySecret'], $config['endpoint']);
-            return new OssStorage($ossClient, $config['bucket'], $config['cdndomain']);
+            $ossClient = new $ossClientClass($config['ossAccessKeyId'], $config['ossAccessKeySecret'], $config['ossEndpoint']);
+            return new OssStorage($ossClient, $config['ossBucket'], $config['ossdomain']);
 
         case 'local':
             return new LocalStorage();
@@ -317,7 +317,8 @@ function getStorage($storage) {
                 'host' => $config['ftpHost'],
                 'port' => $config['ftpPort'],
                 'username' => $config['ftpUsername'],
-                'password' => $config['ftpPassword']
+                'password' => $config['ftpPassword'],
+                'domain' => $config['ftpdomain']
             ]);
 
         default:
